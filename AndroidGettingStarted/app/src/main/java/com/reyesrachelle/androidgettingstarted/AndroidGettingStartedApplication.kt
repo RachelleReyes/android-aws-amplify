80b254c0977ac8497135a7166726a8c996adecfd
Rachelle Reyes <reyes.rachelle@uabc.edu.mx>
2022-11-09 20:51:14 -0800

    Create Android app and initialize Amplify

---
+++ AndroidGettingStarted/app/src/main/java/com/reyesrachelle/androidgettingstarted/AndroidGettingStartedApplication.kt
@@ -0,0 +1,13 @@
+package com.reyesrachelle.androidgettingstarted
+
+import android.app.Application
+
+class AndroidGettingStartedApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // initialize Amplify when application is starting
+        Backend.initialize(applicationContext)
+    }
+}
